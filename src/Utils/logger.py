eed1645edcf604508b02dcc7b8b73461f6f96a64
Andrea <valentea78@gmail.com>
2022-06-22 22:22:06 +0100

    fixed logger test

--- src/Utils/logger.py
+++ src/Utils/logger.py
@@ -1,3 +1,7 @@
+"""
+logger caller
+"""
+
 import logging
 import sys
 from datetime import datetime
@@ -14,20 +18,21 @@ def get_the_logger(log_name: str, logs_path: str, run_date: datetime) -> logging
     """
 
     today = run_date.strftime("%Y-%m-%d")
-    dir_date = os.path.join(logs_path, today)
-    dir_date_name = os.path.join(dir_date, log_name)
+    # dir_date = os.path.join(logs_path, today)
+    # dir_date_name = os.path.join(dir_date, log_name)
 
     if not os.path.exists(logs_path):
         os.mkdir(logs_path)
 
-    if not os.path.exists(dir_date):
-        os.mkdir(dir_date)
+    # if not os.path.exists(dir_date):
+    #     os.mkdir(dir_date)
 
     logging.basicConfig(
-        format=f"%(levelname)s\t{today}\t%(asctime)s\t%(name)s"f"\t%(module)s\t%(funcName)s\t(lineno)d\t%(message)s",
+        format=f"%(levelname)s\t{today}\t%(asctime)s\t%(name)s"
+        f"\t%(module)s\t%(funcName)s\t(lineno)d\t%(message)s",
         datefmt="%H:%M:%S",
         level=logging.INFO,
-        stream=sys.stdout
+        stream=sys.stdout,
     )
 
     proj_logger = logging.getLogger(log_name)
