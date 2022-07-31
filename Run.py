c9852a386ad7e068735bf602ed35d63761fb558e
Andrea <valentea78@gmail.com>
2022-07-31 21:44:48 +0200

    added run.py and project paths

--- Run.py
+++ Run.py
@@ -1,3 +1,13 @@
-print('===================')
+"""
+Main script that controls the whole project
+"""
+
+import argparse
+
+print("===================")
 print("RUN ANALYZE PROJECT")
 print("===================")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
---
+++ config/project_paths.py
@@ -0,0 +1,9 @@
+"""
+Project paths
+"""
+
+import os
+
+project_root = os.path.dirname(os.path.dirname(__file__))
+
+MAIN_CONFIG_PATH = "D:/PycharmProjectsDD/NewRunAnalyzer/config/main_config.yaml"
